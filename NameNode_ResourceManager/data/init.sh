0843a9bb1498930c8e7e77430d3e39dcd29b69ba
maany <imptodefeat@gmail.com>
2019-04-08 20:16:15 +0200

    fix init scripts

--- NameNode_ResourceManager/data/init.sh
+++ NameNode_ResourceManager/data/init.sh
@@ -8,8 +8,14 @@ cp -f /data/yarn-site.xml /root/hadoop/etc/hadoop/
 cp -f /data/slaves /root/hadoop/etc/hadoop/
 echo "Formatting HDFS"
 hdfs namenode -format
+
+echo "Adding known_hosts"
+ssh-keyscan -H 192.0.2.12 >> ~/.ssh/known_hosts
+ssh-keyscan -H 192.0.2.13 >> ~/.ssh/known_hosts
+
 echo "Starting HDFS"
 start-dfs.sh
 echo "HDFS report"
 hdfs dfsadmin -report
+
 echo "All Done!"
--- Worker/data/init.sh
+++ Worker/data/init.sh
@@ -6,4 +6,6 @@ cp /data/hdfs-site.xml /root/hadoop/etc/hadoop/
 cp /data/mapred-site.xml /root/hadoop/etc/hadoop/
 cp -f /data/yarn-site.xml /root/hadoop/etc/hadoop/
 cp -f /data/slaves /root/hadoop/etc/hadoop/
+echo "export PATH=$PATH" >> ~/.bashrc
+echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
 echo "All Done!"
