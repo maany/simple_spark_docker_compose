f09331ebb39f06d006325d55c9e4bdeb99df1063
maany <imptodefeat@gmail.com>
2019-04-08 19:18:51 +0200

    final hdfs config

--- NameNode_ResourceManager/data/hadoop-env.sh
+++ NameNode_ResourceManager/data/hadoop-env.sh
@@ -1,1 +1,1 @@
-export JAVA_HOME=${JAVA_HOME}
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.201.b09-2.el7_6.x86_64/jre
--- NameNode_ResourceManager/data/init.sh
+++ NameNode_ResourceManager/data/init.sh
@@ -6,6 +6,10 @@ cp /data/hdfs-site.xml /root/hadoop/etc/hadoop/
 cp /data/mapred-site.xml /root/hadoop/etc/hadoop/
 cp -f /data/yarn-site.xml /root/hadoop/etc/hadoop/
 cp -f /data/slaves /root/hadoop/etc/hadoop/
-"Formatting HDFS"
+echo "Formatting HDFS"
 hdfs namenode -format
+echo "Starting HDFS"
+start-dfs.sh
+echo "HDFS report"
+hdfs dfsadmin -report
 echo "All Done!"
