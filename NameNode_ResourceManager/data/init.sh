a38a99665f4c13da1b9dc96c22e62652feebc2ae
maany <imptodefeat@gmail.com>
2019-04-08 20:54:57 +0200

    expose yarn, create hdfs directories

--- NameNode_ResourceManager/data/init.sh
+++ NameNode_ResourceManager/data/init.sh
@@ -15,7 +15,22 @@ ssh-keyscan -H 192.0.2.13 >> ~/.ssh/known_hosts
 
 echo "Starting HDFS"
 start-dfs.sh
+
+echo "Start YARN"
+start-yarn.sh
+
 echo "HDFS report"
 hdfs dfsadmin -report
+
+echo "YARN nodes"
+yarn node -list
+
+echo "Adding directories to HDFS"
+mkdir /books
+wget -O /books/alice.txt https://www.gutenberg.org/files/11/11-0.txt  
+wget -O /books/holmes.txt https://www.gutenberg.org/ebooks/1661.txt.utf-8
+wget -O /books/frankenstein.txt https://www.gutenberg.org/ebooks/84.txt.utf-8
+hdfs dfs -mkdir -p /user/maany/books
+hdfs dfs -put /books/alice.txt /books/holmes.txt /books/frankenstein.txt /user/maany/books
 
 echo "All Done!"
